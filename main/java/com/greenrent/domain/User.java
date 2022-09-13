7d7d8d85580c13bb5ce5ada5d5c2268875f184fa
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-14 00:32:30 +0200

    Update User.java

--- main/java/com/greenrent/domain/User.java
+++ main/java/com/greenrent/domain/User.java
@@ -1,4 +1,47 @@
 package com.greenrent.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String firstName;
+
+    @Column(length = 50, nullable = false)
+    private String lastName;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 120, nullable = false)
+    private String password;
+
+    @Column(length = 15, nullable = false)
+    private String phoneNumber;
+
+    @Column(length = 100, nullable = false)
+    private String address;
+
+    @Column(length = 15, nullable = false)
+    private String zipCode;
+
+    @Column(nullable = false)
+    private Boolean builtIn = false;
+
 }
