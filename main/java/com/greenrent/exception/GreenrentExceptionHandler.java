0b1fe0cb8fa3e032ef4d0c03220bffe28c751f94
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-14 00:13:55 +0200

    Update GreenrentExceptionHandler.java

--- main/java/com/greenrent/exception/GreenrentExceptionHandler.java
+++ main/java/com/greenrent/exception/GreenrentExceptionHandler.java
@@ -1,12 +1,17 @@
 package com.greenrent.exception;
 
 import com.greenrent.exception.message.ApiResponseError;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GreenrentExceptionHandler extends ResponseEntityExceptionHandler {
@@ -24,6 +29,21 @@ public class GreenrentExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ConflictException.class)
     protected ResponseEntity<Object> handleConflictException(ConflictException ex, WebRequest request) {
         ApiResponseError error = new ApiResponseError(HttpStatus.CONFLICT, ex.getMessage(), request.getDescription(false));
+        return buildresponseEntity(error);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    protected ResponseEntity<Object> handleBadRequestException(BadRequestException ex, WebRequest request) {
+        ApiResponseError error = new ApiResponseError(HttpStatus.BAD_REQUEST, ex.getMessage(), request.getDescription(false));
+        return buildresponseEntity(error);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, 
+                                                                  HttpStatus status, WebRequest request) {
+
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.toList());
+        ApiResponseError error = new ApiResponseError(HttpStatus.BAD_REQUEST, errors.get(0).toString(), request.getDescription(false));
         return buildresponseEntity(error);
     }
 }
