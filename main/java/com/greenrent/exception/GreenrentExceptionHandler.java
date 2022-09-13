8d87c64f4f67b18df39366717ce392794ff6b64e
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-13 23:16:10 +0200

    Add files via upload

---
+++ main/java/com/greenrent/exception/GreenrentExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.greenrent.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GreenrentExceptionHandler extends ResponseEntityExceptionHandler {
+}
