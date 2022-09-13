57f93a1dce582bbddd537db5eac3ac84339eb012
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-14 00:46:44 +0200

    Update Role.java

--- main/java/com/greenrent/domain/Role.java
+++ main/java/com/greenrent/domain/Role.java
@@ -1,4 +1,28 @@
 package com.greenrent.domain;
 
+import com.greenrent.domain.enums.RoleType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_role")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private RoleType name;
+
+
 }
