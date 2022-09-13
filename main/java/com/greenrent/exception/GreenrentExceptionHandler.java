bf764beca756c909132fe06a724f41c25e8c1a4b
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-13 23:41:22 +0200

    Update GreenrentExceptionHandler.java

--- main/java/com/greenrent/exception/GreenrentExceptionHandler.java
+++ main/java/com/greenrent/exception/GreenrentExceptionHandler.java
@@ -1,8 +1,23 @@
 package com.greenrent.exception;
 
+import com.greenrent.exception.message.ApiResponseError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GreenrentExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private ResponseEntity<Object> buildresponseEntity(ApiResponseError error) {
+        return new ResponseEntity<>(error, error.getStatus());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    protected ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ApiResponseError error = new ApiResponseError(HttpStatus.NOT_FOUND, ex.getMessage(), request.getDescription(false));
+        return buildresponseEntity(error);
+    }
 }
