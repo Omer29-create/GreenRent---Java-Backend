fd0b23b2345d55aa9edaf448b2c413bedce0660e
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-26 21:59:16 +0200

    Update WebSecurityConfig.java

--- main/java/com/greenrent/security/WebSecurityConfig.java
+++ main/java/com/greenrent/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.greenrent.security;
 
+import com.greenrent.security.jwt.AuthTokenFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +32,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 authorizeRequests().antMatchers("/register", "/login").permitAll().
                 anyRequest().authenticated();
 
-        // http.addFilterBefore(null, null);
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
     }
 
     @Bean
