7ea88ebf943cf38af1fe2f1147fc9d68987928f3
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-10-12 16:08:31 +0200

    Update RegisterRequest.java

--- main/java/com/greenrent/dto/RegisterRequest.java
+++ main/java/com/greenrent/dto/RegisterRequest.java
@@ -1,5 +1,27 @@
 package com.greenrent.dto;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
