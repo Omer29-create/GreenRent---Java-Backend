cbb0e4bf84df1f42c3a46a495518b818047a946b
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-10-26 22:20:42 +0200

    Update ContactMessageService.java

--- main/java/com/greenrent/service/ContactMessageService.java
+++ main/java/com/greenrent/service/ContactMessageService.java
@@ -14,10 +14,6 @@ package com.greenrent.service;
 
 
 
-
-
-
-
 import com.greenrent.domain.ContactMessage;
 import com.greenrent.exception.ResourceNotFoundException;
 import com.greenrent.exception.message.ErrorMessage;
