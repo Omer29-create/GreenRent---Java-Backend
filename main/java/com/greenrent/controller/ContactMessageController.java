a7373d4036bbe17eed89020e2cc184e5ecb4ebfd
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-11-03 10:18:36 +0100

    Update ContactMessageController.java

--- main/java/com/greenrent/controller/ContactMessageController.java
+++ main/java/com/greenrent/controller/ContactMessageController.java
@@ -1,5 +1,32 @@
 package com.greenrent.controller;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import com.greenrent.domain.ContactMessage;
 import com.greenrent.service.ContactMessageService;
 import lombok.AllArgsConstructor;
