782f6c311e3364ac65d0b7af9ad3f86a234fb8d5
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-11-11 23:03:41 +0100

    Update AuthTokenFilter.java

--- main/java/com/greenrent/security/jwt/AuthTokenFilter.java
+++ main/java/com/greenrent/security/jwt/AuthTokenFilter.java
@@ -3,9 +3,6 @@ package com.greenrent.security.jwt;
 
 
 
-
-
-
 import com.greenrent.domain.User;
 import com.greenrent.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
