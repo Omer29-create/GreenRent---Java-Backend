5d80f6f5df4d94cff61975ebb28e734a61d69f27
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-11-12 22:57:35 +0100

    Update WebSecurityConfig.java

--- main/java/com/greenrent/security/WebSecurityConfig.java
+++ main/java/com/greenrent/security/WebSecurityConfig.java
@@ -12,10 +12,6 @@ package com.greenrent.security;
 
 
 
-
-
-
-
 import com.greenrent.security.jwt.AuthTokenFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
