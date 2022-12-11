b8b6164d99db4110df8818c0c7e1708225fb4ed6
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-12-11 02:04:42 +0100

    Update RoleRepository.java

--- main/java/com/greenrent/repository/RoleRepository.java
+++ main/java/com/greenrent/repository/RoleRepository.java
@@ -5,8 +5,6 @@ package com.greenrent.repository;
 
 
 
-
-
 import com.greenrent.domain.Role;
 import com.greenrent.domain.enums.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
